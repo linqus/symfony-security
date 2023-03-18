c55b36cb22f4a703afcb0a2808b2dbaed3b0a1b9
Karol Linkiewicz <karol@linkiewicz.pl>
2023-03-18 18:37:18 +0100

    custom authenticator with user provider

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+
+class LoginFormAuthenticator extends AbstractAuthenticator
+{
+    public function supports(Request $request): ?bool
+    {
+        return $request->getPathInfo() === '/login' && $request->isMethod('POST');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+        //dd($email, $password);
+        return new Passport(
+            new UserBadge($email),
+            new CustomCredentials(function($credentials, User $user) {
+                                            dd($credentials, $user);
+                                  }, $password)
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return null;
+    }
+
+//    public function start(Request $request, AuthenticationException $authException = null): Response
+//    {
+//        /*
+//         * If you would like this class to control what happens when an anonymous user accesses a
+//         * protected page (e.g. redirect to /login), uncomment this method and make this class
+//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntrypointInterface.
+//         *
+//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
+//         */
+//    }
+}
