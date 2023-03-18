befb60af7e7b4410d17d10f620d2e07c5f8905be
Karol Linkiewicz <karol@linkiewicz.pl>
2023-03-18 19:59:31 +0100

    authentication process with redirections and error messages in loginform

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -4,20 +4,23 @@ namespace App\Security;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
 class LoginFormAuthenticator extends AbstractAuthenticator
 {
-    public function __construct(private UserRepository $userRepository) {}
+    public function __construct(private UserRepository $userRepository,
+                                private RouterInterface $router) {}
 
     public function supports(Request $request): ?bool
     {
@@ -35,24 +38,25 @@ class LoginFormAuthenticator extends AbstractAuthenticator
                     'email' => $userIdentifier
                 ]);
                 if ($user === null) {
-                    throw new UserNotFoundException('User: '.$userIdentifier);
+                    throw new UserNotFoundException('User not found: '.$userIdentifier);
                 }
                 return $user;
             }),
             new CustomCredentials(function($credentials, User $user) {
-                                            dd($credentials, $user);
+                                    return $credentials === 'tada';
                                   }, $password)
         );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        return null;
+        return new RedirectResponse($this->router->generate('app_homepage'));
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        return null;
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR,$exception);
+        return new RedirectResponse($this->router->generate('app_login'));
     }
 
 //    public function start(Request $request, AuthenticationException $authException = null): Response
