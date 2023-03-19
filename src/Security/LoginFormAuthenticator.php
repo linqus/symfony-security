cf0c5e32e427f3a709a7f321035cb5b7b9f64574
Karol Linkiewicz <karol@linkiewicz.pl>
2023-03-19 09:49:29 +0100

    from AbstractAuthenticator to AbstractLoginFormAuthenticator, cleaning code

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -9,26 +9,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
-use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
-class LoginFormAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator 
 {
     public function __construct(private UserRepository $userRepository,
                                 private RouterInterface $router) {}
-
-    public function supports(Request $request): ?bool
-    {
-        return $request->getPathInfo() === '/login' && $request->isMethod('POST');
-    }
 
     public function authenticate(Request $request): Passport
     {
@@ -58,14 +50,8 @@ class LoginFormAuthenticator extends AbstractAuthenticator implements Authentica
         return new RedirectResponse($this->router->generate('app_homepage'));
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    public function getLoginUrl(Request $request): string
     {
-        $request->getSession()->set(Security::AUTHENTICATION_ERROR,$exception);
-        return new RedirectResponse($this->router->generate('app_login'));
+            return $this->router->generate('app_login');
     }
-
-   public function start(Request $request, AuthenticationException $authException = null): Response
-   {
-        return new RedirectResponse($this->router->generate('app_login'));
-   }
 }
